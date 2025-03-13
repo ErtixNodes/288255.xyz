a64375dc580331d2c28edca5cf3544cd3170d5a8
BasToTheMax <info@bastothemax.nl>
2025-03-13 17:41:14 +0100

    Add sesisons + request logger

--- index.js
+++ index.js
@@ -3,9 +3,31 @@ require('dotenv').config();
 const port = process.env.PORT || 3000;
 
 const express = require('express');
+const session = require('express-session');
 const app = express();
 
 const db = require('./db.js');
+
+app.set('trust proxy', 1);
+
+let secret = process.env.SECRET || 'keyboard cat';
+app.use(session({
+    secret,
+    cookie: {
+        maxAge: 60 * 60 * 1000 * 24 * 30 // 30 days
+    },
+    saveUninitialized: false,
+    resave: false
+}));
+
+app.use(express.json());
+
+let requestCount = 0;
+app.use((req, res, next) => {
+    requestCount++;
+    console.log(`[${String(requestCount).padStart(6, '0')}] [${req.ip}] ${req.method} ${req.url}`);
+    next();
+});
 
 app.use(require('./landing.js'));
 app.use('/dash', require('./dash.js'));
@@ -21,7 +43,7 @@ setInterval(async () => {
         node.serverCount = await db.Server.countDocuments({ node: node.code });
         node.percentage = (node.serverCount / node.serverLimit) * 100;
         node.percentage = Math.round(node.percentage);
-        
+
         node.isFull = node.serverCount >= node.serverLimit;
         
         await node.save();
