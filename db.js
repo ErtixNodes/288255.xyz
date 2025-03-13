2c68d614a642748acd8d7b51a9a89abcd28bc226
BasToTheMax <info@bastothemax.nl>
2025-03-13 17:34:53 +0100

    Add db + node calculation

--- db.js
+++ db.js
@@ -2,18 +2,30 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO);
 
-const User = new mongoose.Model('User', {
+const User = new mongoose.model('User', {
     email: String,
     password: String,
     role: String,
     balance: Number
 });
 
-const Server = new mongoose.Model('Server', {
+const Server = new mongoose.model('Server', {
     userID: mongoose.Types.ObjectId,
     name: String,
     port: Number,
     node: String
 });
 
-module.exports = { User, Server };
+const Node = new mongoose.model('Node', {
+    code: String,
+    ip: String,
+    
+    serverCount: Number,
+    serverLimit: Number,
+    percentage: Number,
+    
+    isFull: Boolean,
+    isAvailable: Boolean
+});
+
+module.exports = { User, Server, Node };
--- index.js
+++ index.js
@@ -5,9 +5,25 @@ const port = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 
+const db = require('./db.js');
+
 app.use(require('./landing.js'));
 app.use('/dash', require('./dash.js'));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+setInterval(async () => {
+    let nodes = await db.Node.find();
+    for (let node of nodes) {
+        // Update node status
+        node.serverCount = await db.Server.countDocuments({ node: node.code });
+        node.percentage = (node.serverCount / node.serverLimit) * 100;
+        node.percentage = Math.round(node.percentage);
+        
+        node.isFull = node.serverCount >= node.serverLimit;
+        
+        await node.save();
+    }
+}, 5 * 1000);
